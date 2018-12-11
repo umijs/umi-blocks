523ab4eef02637e4205733af24e7b15e38394890
依鹭 <jingcao.cjc@antfin.com>
2018-12-11 21:05:27 +0800

    move mock to src

---
+++ ant-design-pro/Exception404/src/_mock.js
@@ -0,0 +1,11 @@
+export default {
+  'GET /api/404': (req, res) => {
+    res.status(404).send({
+      timestamp: 1513932643431,
+      status: 404,
+      error: 'Not Found',
+      message: 'No message available',
+      path: '/base/category/list/2121212',
+    });
+  },
+};
---
+++ ant-design-pro/Exception500/src/_mock.js
@@ -0,0 +1,11 @@
+export default {
+  'GET /api/500': (req, res) => {
+    res.status(500).send({
+      timestamp: 1513932555104,
+      status: 500,
+      error: 'error',
+      message: 'error',
+      path: '/base/category/list',
+    });
+  },
+};
