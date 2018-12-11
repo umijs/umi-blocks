168155621d9ad77d5269848a58d42d57cb202b3a
依鹭 <jingcao.cjc@antfin.com>
2018-12-11 19:59:22 +0800

    move mock to src

---
+++ ant-design-pro/Exception403/src/_mock.js
@@ -0,0 +1,11 @@
+export default {
+  'GET /api/403': (req, res) => {
+    res.status(403).send({
+      timestamp: 1513932555104,
+      status: 403,
+      error: 'Unauthorized',
+      message: 'Unauthorized',
+      path: '/base/category/list',
+    });
+  },
+};
