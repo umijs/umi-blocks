63b92648d9604ccd60f160ef21a7e23e1c1fd304
kdot <441002295@qq.com>
2018-12-13 17:49:35 +0800

    convert TableList to a umi block (#9)
    * convert TableList to umi-blocks
    
    * remove useless mock data and service

---
+++ ant-design-pro/TableList/src/service.js
@@ -0,0 +1,36 @@
+import { stringify } from 'qs';
+import request from 'umi-request';
+
+export async function queryRule(params) {
+  return request(`/api/rule?${stringify(params)}`);
+}
+
+export async function removeRule(params) {
+  return request('/api/rule', {
+    method: 'POST',
+    body: {
+      ...params,
+      method: 'delete',
+    },
+  });
+}
+
+export async function addRule(params) {
+  return request('/api/rule', {
+    method: 'POST',
+    body: {
+      ...params,
+      method: 'post',
+    },
+  });
+}
+
+export async function updateRule(params) {
+  return request('/api/rule', {
+    method: 'POST',
+    body: {
+      ...params,
+      method: 'update',
+    },
+  });
+}
