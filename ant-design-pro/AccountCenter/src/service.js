f24908705856d5684e7e68c7426bd97fbfbb3c8e
陈帅 <qixian.cs@outlook.com>
2019-06-19 11:30:55 +0800

    use pro submodule

--- ant-design-pro/AccountCenter/src/service.js
+++
@@ -1,9 +0,0 @@
-import request from 'umi-request';
-
-export async function query() {
-  return request('/api/BLOCK_NAME/users');
-}
-
-export async function queryCurrent() {
-  return request('/api/BLOCK_NAME/currentUser');
-}
--- ant-design-pro/UserRegister/src/service.js
+++
@@ -1,8 +0,0 @@
-import request from 'umi-request';
-
-export async function fakeRegister(params) {
-  return request('/api/BLOCK_NAME/register', {
-    method: 'POST',
-    data: params,
-  });
-}
