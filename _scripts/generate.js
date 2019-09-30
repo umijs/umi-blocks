f035d04e65597518b122647ddd95d621e0bd77f8
sorrycc <sorrycc@gmail.com>
2019-09-30 13:35:19 +0800

    chore: generate features

--- _scripts/generate.js
+++ _scripts/generate.js
@@ -2,6 +2,34 @@ const { readdirSync, readFileSync, writeFileSync, existsSync } = require('fs');
 const { join, basename } = require('path');
 const mkdirp = require('mkdirp');
 
+function haveDependencies(pkg, depName) {
+  if (pkg.dependencies && pkg.dependencies[depName]) {
+    return true;
+  }
+  if (pkg.devDependencies && pkg.devDependencies[depName]) {
+    return true;
+  }
+  return false;
+}
+
+const EXT_NAMES = ['.tsx', '.ts', '.jsx', '.js'];
+
+function getFile(cwd, fileName) {
+  for (const ext of EXT_NAMES) {
+    const file = join(cwd, `${fileName}${ext}`);
+    if (existsSync(file)) {
+      return file;
+    }
+  }
+}
+
+function haveImport(cwd, name) {
+  const indexFile = getFile(cwd, 'src/index');
+  if (!indexFile) return false;
+  const content = readFileSync(indexFile, 'utf-8');
+  return content.includes(`'${name}'`) || content.includes(`"${name}"`);
+}
+
 function parseJSON(root) {
   const dirs = readdirSync(root);
   const type = basename(root);
@@ -11,6 +39,13 @@ function parseJSON(root) {
     const pkg = require(join(absDirPath, 'package.json'));
     const url = `https://github.com/umijs/umi-blocks/tree/master/${type}/${dir}`;
     const img = `https://github.com/umijs/umi-blocks/blob/master/${type}/${dir}/snapshot.png?raw=true`;
+    const features = [];
+    if (haveDependencies(pkg, 'antd') || haveImport(absDirPath, 'antd')) {
+      features.push('antd');
+    }
+    if (getFile(absDirPath, 'src/model')) {
+      features.push('dva');
+    }
     memo.push({
       name: pkg.name,
       description: pkg.description,
@@ -18,7 +53,7 @@ function parseJSON(root) {
       tags: [],
       img,
       previewUrl: '',
-      features: [],
+      features,
     });
     return memo;
   }, []);
