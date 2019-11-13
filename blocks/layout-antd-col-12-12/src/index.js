7ffc8b4b38bc46c77033d11026059be4f748dcbf
sorrycc <sorrycc@gmail.com>
2019-11-13 16:08:10 +0800

    add blocks/layout-antd-col-12-12

---
+++ blocks/layout-antd-col-12-12/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+
+export default () => {
+  return (
+    <Row>
+      <Col span={12}>INSERT_BLOCK_PLACEHOLDER:Col 12</Col>
+      <Col span={12}>INSERT_BLOCK_PLACEHOLDER:Col 12</Col>
+    </Row>
+  );
+}
