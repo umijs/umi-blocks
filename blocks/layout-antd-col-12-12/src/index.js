70d4167aec868135bf9192ae189333b21de00758
陈帅 <qixian.cs@outlook.com>
2019-11-25 17:26:04 +0800

    new layout block style

--- blocks/layout-antd-col-12-12/src/index.js
+++ blocks/layout-antd-col-12-12/src/index.js
@@ -4,8 +4,12 @@ import { Row, Col } from 'antd';
 export default () => {
   return (
     <Row>
-      <Col span={12} style={{ minHeight: '20px', background: '#fecccc' }}>INSERT_BLOCK_PLACEHOLDER:Col 12</Col>
-      <Col span={12} style={{ minHeight: '20px', background: '#82caff' }}>INSERT_BLOCK_PLACEHOLDER:Col 12</Col>
+      <Col span={12} style={{ minHeight: 400, padding: 16, borderRight: '1px solid #DDD' }}>
+        INSERT_BLOCK_PLACEHOLDER:Col 12
+      </Col>
+      <Col span={12} style={{ minHeight: 400, padding: 16 }}>
+        INSERT_BLOCK_PLACEHOLDER:Col 12
+      </Col>
     </Row>
   );
-}
+};
